9711ac61c90f850f508d90c9b2413d3f614edebb
kandarp dangi <kkandarpk7@gmail.com>
2021-05-05 23:36:32 +0530

    responsive arc

--- src/components/screens/SignIn.js
+++ src/components/screens/SignIn.js
@@ -1,65 +1,65 @@
-import React, { useState, useContext } from "react";
-import { UserContext } from "../../App";
-import Avatar from "@material-ui/core/Avatar";
-import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import TextField from "@material-ui/core/TextField";
-import { Link, useHistory } from "react-router-dom";
-import Grid from "@material-ui/core/Grid";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
-import { CardActionArea } from "@material-ui/core";
-import { NotifyContext } from "../../App";
-import "./signin.css";
+import React, { useState, useContext } from 'react'
+import { UserContext } from '../../App'
+import Avatar from '@material-ui/core/Avatar'
+import Button from '@material-ui/core/Button'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import TextField from '@material-ui/core/TextField'
+import { Link, useHistory } from 'react-router-dom'
+import Grid from '@material-ui/core/Grid'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
+import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core/styles'
+import Container from '@material-ui/core/Container'
+import { CardActionArea } from '@material-ui/core'
+import { NotifyContext } from '../../App'
+import './signin.css'
 
 const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(4),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
   },
   avatar: {
     margin: theme.spacing(1),
     backgroundColor: theme.palette.secondary.main,
   },
   form: {
-    width: "100%", // Fix IE 11 issue.
+    width: '100%', // Fix IE 11 issue.
     marginTop: theme.spacing(1),
   },
   submit: {
     margin: theme.spacing(3, 0, 2),
   },
-}));
+}))
 
 export default function SignIn() {
-  const classes = useStyles();
+  const classes = useStyles()
   // eslint-disable-next-line
-  const { dispatch, url } = useContext(UserContext);
-  const history = useHistory();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [disable, setDisable] = useState(false);
+  const { dispatch, url } = useContext(UserContext)
+  const history = useHistory()
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [disable, setDisable] = useState(false)
   // eslint-disable-next-line
-  const { dispatchNotification, dispatchLoad } = useContext(NotifyContext);
+  const { dispatchNotification, dispatchLoad } = useContext(NotifyContext)
 
-  document.title = "Signin | BlogCode";
+  document.title = 'Signin | BlogCode'
 
   const validateEmail = (value) => {
-    setEmail(value);
-  };
+    setEmail(value)
+  }
 
   const sendData = (e) => {
-    setDisable(true);
-    dispatchLoad({ type: "LOAD" });
-    e.preventDefault();
+    setDisable(true)
+    dispatchLoad({ type: 'LOAD' })
+    e.preventDefault()
     // eslint-disable-next-line
-    fetch(url + "/signin", {
-      method: "post",
+    fetch(url + '/signin', {
+      method: 'post',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({
         email,
@@ -70,78 +70,78 @@ export default function SignIn() {
       .then((data) => {
         if (data.error) {
           dispatchNotification({
-            type: "NOTIFY",
+            type: 'NOTIFY',
             payload: data.error,
-            snacktype: "error",
-          });
-          setDisable(false);
-          dispatchLoad({ type: "LOADOFF" });
+            snacktype: 'error',
+          })
+          setDisable(false)
+          dispatchLoad({ type: 'LOADOFF' })
         } else {
-          localStorage.setItem("jwt", data.token);
-          localStorage.setItem("user", JSON.stringify(data.user));
-          dispatch({ type: "USER", payload: data.user });
+          localStorage.setItem('jwt', data.token)
+          localStorage.setItem('user', JSON.stringify(data.user))
+          dispatch({ type: 'USER', payload: data.user })
           dispatchNotification({
-            type: "NOTIFY",
-            payload: "Welcome to BlogCode",
-            snacktype: "success",
-            snackcolor: "success",
+            type: 'NOTIFY',
+            payload: 'Welcome to BlogCode',
+            snacktype: 'success',
+            snackcolor: 'success',
             duration: 3000,
-          });
-          setDisable(false);
-          dispatchLoad({ type: "LOADOFF" });
-          history.push("/");
+          })
+          setDisable(false)
+          dispatchLoad({ type: 'LOADOFF' })
+          history.push('/')
         }
       })
-      .catch((err) => console.log(err));
-  };
+      .catch((err) => console.log(err))
+  }
 
   return (
     <div>
-      <Container component="main" maxWidth="xs">
+      <Container component='main' maxWidth='xs'>
         <CssBaseline />
         <div className={classes.paper}>
           <Avatar className={classes.avatar}>
             <LockOutlinedIcon />
           </Avatar>
-          <Typography component="h1" variant="h5">
+          <Typography component='h1' variant='h5'>
             Sign in
           </Typography>
           <form
             className={classes.form}
             onSubmit={(e) => sendData(e)}
-            validate="true"
-            autoComplete="off"
+            validate='true'
+            autoComplete='off'
           >
             <TextField
-              color="secondary"
-              variant="outlined"
-              margin="normal"
+              color='secondary'
+              variant='outlined'
+              margin='normal'
               required
               fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
+              id='email'
+              label='Email Address'
+              name='email'
               value={email}
               onChange={(e) => validateEmail(e.target.value)}
-              autoComplete="email"
+              autoComplete='email'
             />
             <TextField
-              id="password"
-              color="secondary"
-              type={"password"}
-              margin="normal"
-              label="password"
-              variant="outlined"
+              id='password'
+              color='secondary'
+              type={'password'}
+              margin='normal'
+              label='password'
+              variant='outlined'
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               required
               fullWidth
             />
             <Button
-              type="submit"
+              type='submit'
               fullWidth
-              variant="contained"
-              color="secondary"
+              variant='contained'
+              color='secondary'
               className={classes.submit}
               disabled={disable}
             >
@@ -149,7 +149,7 @@ export default function SignIn() {
             </Button>
             <Grid container>
               <Grid item xs>
-                <Link to="/reset" variant="body2">
+                <Link to='/reset' variant='body2'>
                   Forgot password?
                 </Link>
               </Grid>
@@ -157,9 +157,9 @@ export default function SignIn() {
                 <CardActionArea>
                   <div
                     onClick={() => {
-                      history.push("/signup");
+                      history.push('/signup')
                     }}
-                    variant="body2"
+                    variant='body2'
                   >
                     {"Don't have an account? Sign Up"}
                   </div>
@@ -170,5 +170,5 @@ export default function SignIn() {
         </div>
       </Container>
     </div>
-  );
+  )
 }
